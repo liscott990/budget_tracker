20aba73115ec0c61769fe7a493637c2d2462349f
liscott990 <lisakanzler92@gmail.com>
2026-06-19 15:06:09 -0700

    Update budget_tracker.py

--- budget_tracker.py
+++ budget_tracker.py
@@ -2,3 +2,24 @@
 # Personal Budget Tracker
 # This program helps track income and expenses, view summaries,
 # and save/load transaction history to a file.
+
+import random
+import json
+import os
+from datetime import date
+
+# -------------------------------------------------------
+# Global list to store all transactions during the session
+# Each transaction is stored as a dictionary
+# -------------------------------------------------------
+transactions = []
+
+# Categories the use can pick from
+INCOME_CATEGORIES = ["Job", "Freelance", "Gift", "Financial aid", "Other"]
+EXPENSE CATEGORIES = ["Food", "Rent", "Transportation", "Entertainment", "School", "Health", "Other"]
+
+# The file where the save/load data goes
+DATA_FILE = "budget_data.json"
+
+
+# -------------------------------------------------------
